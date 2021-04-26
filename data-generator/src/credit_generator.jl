d6bd62bf289e0bc52c5f1e6321754db3235dc2bc
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2021-04-26 16:32:18 +0200

    Code format

--- data-generator/src/credit_generator.jl
+++ data-generator/src/credit_generator.jl
@@ -12,7 +12,7 @@ n = 10000
 @model function individual_attributes()
     income_monthly ~ Gamma(10, 150)
     individual_default_risk ~ Beta(2, 30)
-    application_date ~ TruncatedNormal(500, 200, 1, 365*2)
+    application_date ~ TruncatedNormal(500, 200, 1, 365 * 2)
 end
 
 # Assume business-cycle risk is a function of date and white noise
@@ -25,20 +25,35 @@ business_cycle_params = DataFrame(sample(business_cycle(), NUTS(1000, 0.65), 1))
 period = business_cycle_params[1, :period]
 shift = business_cycle_params[1, :shift]
 
-business_cycle_t(t) = 0.5 * sin( (t - shift) / (period / (2 * pi) )) + 0.5
+business_cycle_t(t) = 0.5 * sin((t - shift) / (period / (2 * pi))) + 0.5
 # business_cycle_with_noise = business_cycle_t.(1:(365*2)) .* rand(TruncatedNormal(0.95, 0.1, 0.01, 1), 365*2)
-business_cycle_with_noise = business_cycle_t.(1:(365*2)) .^ rand(Truncated(Poisson(5), 1, 5), 365*2)
+business_cycle_with_noise =
+    business_cycle_t.(1:(365*2)) .^ rand(Truncated(Poisson(5), 1, 5), 365 * 2)
 business_cycle_default_risk = (rand(Uniform(0.1, 0.5)) .* business_cycle_with_noise)
 # plot(1:(365*2), business_cycle_risk)
 
-business_cycle_df = DataFrame("application_date" => 1:(365*2), "business_cycle_default_risk" => business_cycle_default_risk)
+business_cycle_df = DataFrame(
+    "application_date" => 1:(365*2),
+    "business_cycle_default_risk" => business_cycle_default_risk,
+)
 individuals_df = DataFrame(sample(individual_attributes(), NUTS(1000, 0.65), n))
-individuals_df[!, :application_date] .= round.(individuals_df[!, :application_date], digits=0)
+individuals_df[!, :application_date] .=
+    round.(individuals_df[!, :application_date], digits = 0)
 individuals_df
 
-portfolio_df = leftjoin(individuals_df[!, [:application_date, :individual_default_risk]], business_cycle_df, on=:application_date)
+portfolio_df = leftjoin(
+    individuals_df[!, [:application_date, :individual_default_risk]],
+    business_cycle_df,
+    on = :application_date,
+)
 
-portfolio_df[!, :total_default_risk] = clamp.(portfolio_df[!, :individual_default_risk] .+ portfolio_df[!, :business_cycle_default_risk], 0, 1)
+portfolio_df[!, :total_default_risk] =
+    clamp.(
+        portfolio_df[!, :individual_default_risk] .+
+        portfolio_df[!, :business_cycle_default_risk],
+        0,
+        1,
+    )
 
 portfolio_df[!, :default] = rand.(Bernoulli.(portfolio_df[!, :total_default_risk]))
 
