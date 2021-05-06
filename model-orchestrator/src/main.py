454682bef49f3c1b5263ec9790d18bcccc3f650e
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2021-05-06 15:50:46 +0200

    Update data

--- model-orchestrator/src/main.py
+++ model-orchestrator/src/main.py
@@ -1,64 +1,132 @@
-from dagster import pipeline, solid
 import pandas as pd
+from dagster import pipeline, solid
 from sklearn import linear_model
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import StandardScaler
+from uuid import uuid4
+
+
+def get_raw_data():
+    raw_df = pd.read_parquet("portfolio_df.parquet")
+    # TODO: Move the application id generation to data sim code!
+    raw_df["application_id"] = raw_df.application_date.apply(lambda x: str(uuid4()))
+    return raw_df
 
 
 @solid
-def gather_historical_data(context) -> pd.DataFrame:
+def gather_historical_data(context, epoch_start: int = 0) -> pd.DataFrame:
+    historical_df = get_raw_data()
+    historical_df = historical_df.loc[
+        historical_df.application_date < (epoch_start + 1) * 100
+    ].copy()
+    return historical_df
+
+
+@solid
+def empty_dataframe(_) -> pd.DataFrame:
     return pd.DataFrame()
 
 
 @solid
-def fetch_training_data(context, new_data: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame()
+def merge_data(historical_data, new_data) -> pd.DataFrame:
+    return historical_data.append(new_data)
 
 
 @solid
-def fetch_model_pipeline(_):
-    return None
+def fetch_model_pipeline(_) -> Pipeline:
+    column_trans = ColumnTransformer(
+        [
+            ("individual_default_risk", StandardScaler(), ["individual_default_risk"]),
+            (
+                "business_cycle_default_risk",
+                StandardScaler(),
+                ["business_cycle_default_risk"],
+            ),
+        ],
+        remainder="drop",
+    )
+    model_pipeline = make_pipeline(column_trans, linear_model.LogisticRegression())
+    return model_pipeline
 
 
 @solid
-def train_model(context, training_data: pd.DataFrame, model_pipeline):
+def train_model(
+    context, training_df: pd.DataFrame, model_pipeline: Pipeline
+) -> Pipeline:
     """
     training_data: data collected from previous loans granted, as pd.DataFrame
     model: machine learning model (pipeline) which can be applied to training data
     """
     # Replace this!
-    reg = linear_model.LinearRegression()
-    reg.fit([[0, 0], [1, 1], [2, 2]], [0, 1, 2])
-    return reg
+    model_pipeline.fit(training_df, training_df["default"])
+    return model_pipeline
 
 
 @solid
-def collect_applications(_)-> pd.DataFrame:
+def collect_applications(context, epoch: int) -> pd.DataFrame:
     """
     Collects applications for new loans from customers
     """
-    return pd.DataFrame()
+
+    applications_df = get_raw_data()
+    applications_df = applications_df.loc[
+        (applications_df.application_date >= epoch * 100)
+        & (applications_df.application_date < (epoch + 1) * 100)
+    ].copy()
+    applications_df.reset_index(inplace=True, drop=True)
+    return applications_df
 
 
 @solid
-def choose_business_portfolio(context, applications_df: pd.DataFrame, model)-> pd.DataFrame:
+def choose_business_portfolio(
+    context,
+    applications_df: pd.DataFrame,
+    model_pipeline: Pipeline,
+    n_loan_budget: int = 100,
+) -> pd.DataFrame:
     """
     Decide whom to grant loans to (for profit)
 
     applications: pd.DataFrame
     model: machine learning model (pipeline) which can be applied to applications, based on training data
     """
-    return pd.DataFrame()
+
+    applications_df["est_default_prob"] = pd.DataFrame(
+        model_pipeline.predict_proba(applications_df)
+    ).loc[:, 1]
+
+    assert (
+        applications_df.est_default_prob.isna().sum() == 0
+    ), "Some estimated default probabilities NaN"
+
+    business_portfolio_df = applications_df.loc[
+        applications_df["est_default_prob"].rank(method="first") <= n_loan_budget
+    ][["application_id"]].reset_index(drop=True)
+
+    return business_portfolio_df
 
 
 @solid
 def choose_research_portfolio(
-    context, applications_df: pd.DataFrame, business_portfolio_df: pd.DataFrame, model
-)-> pd.DataFrame:
+    context,
+    applications_df: pd.DataFrame,
+    business_portfolio_df: pd.DataFrame,
+    model_pipeline: Pipeline,
+    n_research_budget: int = 20,
+) -> pd.DataFrame:
     """
     Decide whom to grant loans to (for research / profit in subsequent rounds)
 
     business_portfolio: {"application_id", "loan_granted"} as pd.DataFrame
     """
-    return pd.DataFrame()
+    research_portfolio_df =  applications_df[
+        ~applications_df.application_id.isin(
+            business_portfolio_df.application_id.tolist()
+        )
+    ].sample(n_research_budget)[["application_id"]].reset_index(drop=True)
+
+    return research_portfolio_df
 
 
 @solid
@@ -67,18 +135,27 @@ def grant_credit(
     applications_df: pd.DataFrame,
     business_portfolio_df: pd.DataFrame,
     research_portfolio_df: pd.DataFrame,
-)-> pd.DataFrame:
+) -> pd.DataFrame:
     """
     Grant credit to individuals and observe outcomes
     """
-    return pd.DataFrame()
+    applications_df["portfolio"] = "rejected"
+    applications_df.loc[applications_df.application_id.isin(business_portfolio_df.application_id.tolist()), "portfolio"] = "business"
+    applications_df.loc[applications_df.application_id.isin(research_portfolio_df.application_id.tolist()), "portfolio"] = "research"
+
+    applications_df["credit_granted"] = applications_df.portfolio != "rejected"
+    portfolio_df = applications_df[["application_id", "portfolio", "credit_granted"]]
+    return portfolio_df
 
 
 @solid
-def observe_outcomes(context, portfolio_df: pd.DataFrame)-> pd.DataFrame:
+def observe_outcomes(context, portfolio_df: pd.DataFrame) -> pd.DataFrame:
     """
     Observe outcomes to granted credit.
     """
+
+    raw_data = get_raw_data()[["application_id", "default"]]
+    pd.merge(portfolio_df, raw_data, on="application_id", how="left")
     return pd.DataFrame()
 
 
@@ -87,12 +164,11 @@ def active_learning_credit_pipeline():
     historical_data = gather_historical_data()
 
     for t in range(10):
-        training_df = fetch_training_data(historical_data)
         model_pipeline = fetch_model_pipeline()
 
-        trained_model = train_model(training_df, model_pipeline)
+        trained_model = train_model(historical_data, model_pipeline)
 
-        applications_df = collect_applications()
+        applications_df = collect_applications(t)
 
         business_portfolio_df = choose_business_portfolio(
             applications_df, trained_model
@@ -106,4 +182,5 @@ def active_learning_credit_pipeline():
             applications_df, business_portfolio_df, research_portfolio_df
         )
 
-        historical_data = observe_outcomes(portfolio_df)
+        new_data = observe_outcomes(portfolio_df)
+        historical_data = merge_data(historical_data, new_data)
