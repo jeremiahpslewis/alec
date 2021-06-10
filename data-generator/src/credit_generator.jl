300b9b4ed2180a12f4f27279e8782d25ee75fed5
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2021-06-10 18:46:51 +0200

    Rework synthetic data

--- data-generator/src/credit_generator.jl
+++ data-generator/src/credit_generator.jl
@@ -20,47 +20,33 @@ function generate_synthetic_data(n_applications)
     # Credit Default Dataset with Business Cycle Effects
     # Assume income, personal default risk, application rate are independent
     @model function individual_attributes()
-        income_monthly ~ Gamma(10, 150)
-        individual_default_risk ~ Beta(2, 30)
+        income_based_risk ~ TruncatedNormal(0.3, 0.2, 0, 1)
+        assets_based_risk ~ TruncatedNormal(0.7, 0.2, 0, 1)
+        income_over_assets_individual_risk_weight ~ TruncatedNormal(0.5, 0.4, 0, 1)
     end
 
-    # Assume business-cycle risk is a function of date and white noise
-    @model function business_cycle()
-        shift ~ Normal(50, 200)
-        period ~ Normal(365, 30)
-    end
-
-    business_cycle_params = DataFrame(sample(business_cycle(), NUTS(1000, 0.65), 1))
-    period = business_cycle_params[1, :period]
-    shift = business_cycle_params[1, :shift]
-
-    business_cycle_t(t) = 0.5 * sin((t - shift) / (period / (2 * pi))) + 0.5
-    # business_cycle_with_noise = business_cycle_t.(1:(365*2)) .* rand(TruncatedNormal(0.95, 0.1, 0.01, 1), 365*2)
-    business_cycle_with_noise =
-        business_cycle_t.((1:n_periods) * 365) .^ rand(Truncated(Poisson(5), 1, 5), n_periods)
-    business_cycle_default_risk = (rand(Uniform(0.1, 0.5)) .* business_cycle_with_noise)
-    # plot(1:10, business_cycle_risk)
+    
 
     business_cycle_df = DataFrame(
         "application_date" => 1:n_periods,
-        "business_cycle_default_risk" => business_cycle_default_risk,
+        "income_over_assets_cycle_risk_weight" => 1 .- 0.9.^(1:n_periods),
     )
+
     individuals_df = DataFrame(sample(individual_attributes(), NUTS(1000, 0.65), n_applications))
     individuals_df[!, :application_date] .= repeat(1:n_periods, n_applications_per_period)
 
     portfolio_df = leftjoin(
-        individuals_df[!, [:application_date, :individual_default_risk]],
+        individuals_df[!, [:application_date, :income_based_risk, :assets_based_risk, :income_over_assets_individual_risk_weight]],
         business_cycle_df,
         on = :application_date,
     )
 
-    portfolio_df[!, :total_default_risk] =
-        clamp.(
-            portfolio_df[!, :individual_default_risk] .+
-            portfolio_df[!, :business_cycle_default_risk],
-            0,
-            1,
-        )
+    portfolio_df[!, :income_over_assets_gross_risk_weight] =
+        (portfolio_df[!, :income_over_assets_individual_risk_weight].^2 .+
+        portfolio_df[!, :income_over_assets_cycle_risk_weight].^2).^0.5    
+    
+    portfolio_df[!, :total_default_risk] = (portfolio_df[!, :income_over_assets_gross_risk_weight] .* portfolio_df[!, :income_based_risk]) .+
+    ((1 .- portfolio_df[!, :income_over_assets_gross_risk_weight]) .* portfolio_df[!, :income_based_risk])
 
     portfolio_df[!, :default] = rand.(Bernoulli.(portfolio_df[!, :total_default_risk]))
 
