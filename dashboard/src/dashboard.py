b3a306aba5a49e7bc11f9db3ff1f4c93e4d73458
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2021-08-21 17:50:20 +0200

    Update dependencies

--- dashboard/src/dashboard.py
+++ dashboard/src/dashboard.py
@@ -16,7 +16,7 @@ s3_alec = s3.Bucket("alec")
 simulation_ids = []
 scenario_ids = []
 
-if False:
+if True:
     for i in ["applications", "outcomes", "portfolios", "scenarios"]:
         file_paths_tmp = [f.key for f in s3_alec.objects.filter(Prefix=f"{i}/")]
         simulation_ids_tmp = [
