7d6d036a595f5e2772a56b5a58294569ed5ca2bd
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2021-08-27 23:14:56 +0200

    Use test script to cross check logic...

---
+++ data-generator/src/active_learning_scrap.jl
@@ -0,0 +1,54 @@
+using Soss
+using MeasureTheory
+using StatsPlots
+using Parquet
+using DataFrames
+using Chain
+using UUIDs
+using DataFrameMacros
+using GLM
+
+n_applications_per_period = 250
+
+
+loan_data_generator = @model income_based_risk_var, asset_based_risk_var begin
+    income_based_risk ~ MeasureTheory.Normal(0, income_based_risk_var)
+    asset_based_risk ~ MeasureTheory.Normal(0, asset_based_risk_var)
+    idiosyncratic_individual_risk ~ MeasureTheory.Normal(0, 0.1)
+    total_default_risk_log_odds = idiosyncratic_individual_risk + income_based_risk + asset_based_risk
+    total_default_risk = logistic(total_default_risk_log_odds)
+    default ~ MeasureTheory.Bernoulli(total_default_risk)
+end
+
+income_based_risk_var = [0.1, 0.1, 0.1, 0.1, 0.1, 4, 4, 4, 4, 4]
+asset_based_risk_var = [4, 4, 4, 4, 4, 0.1, 0.1, 0.1, 0.1, 0.1]
+
+business_cycle_df = DataFrame(
+    "income_based_risk_var" => income_based_risk_var,
+    "asset_based_risk_var" => asset_based_risk_var,
+)
+
+n_periods = nrow(business_cycle_df)
+business_cycle_df[!, "application_date"] = 2020:(2020 + (n_periods - 1))
+
+
+portfolio_df = DataFrame()
+for x in eachrow(business_cycle_df)
+    one_cycle_df = DataFrame(rand(loan_data_generator(x.income_based_risk_var, x.asset_based_risk_var), n_applications_per_period))
+    one_cycle_df = @chain one_cycle_df begin
+        @transform(:application_date = x.application_date,
+                   :default = :default * 1, # convert bool to int
+                   :income_based_risk_var = x.income_based_risk_var,
+                   :asset_based_risk_var = x.asset_based_risk_var,
+                   )
+    end
+    append!(portfolio_df, one_cycle_df)
+end
+
+
+portfolio_df = generate_synthetic_data(n_applications_per_period)
+fm = @formula(default ~ income_based_risk + asset_based_risk)
+
+portfolio_df_subset = @chain portfolio_df begin
+    @subset(:application_date == 2020)
+end;
