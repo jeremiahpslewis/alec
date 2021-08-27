205d26e8c0e88065cd0770e6d724fcbd21f84f51
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2021-08-27 20:28:51 +0200

    Simplify model

--- model-orchestrator/src/main.py
+++ model-orchestrator/src/main.py
@@ -28,7 +28,7 @@ simulation_metadata = [
     "application_date",
 ]
 
-X_vars = ["income_based_risk", "age", "age_squared"]
+X_vars = ["income_based_risk", "age_squared"]
 y_var = ["default"]
 
 full_application_col_set = [*simulation_indices, *simulation_metadata, *X_vars]
