5ed7aeae7b0b22257b53e96d4aea97d407e1b5d0
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2021-08-27 11:42:49 +0200

    Fix param

--- data-generator/src/credit_generator.jl
+++ data-generator/src/credit_generator.jl
@@ -16,7 +16,7 @@ mode = "test"
 
 if mode == "test"
     n_simulations = 35
-    n_applications_per_period = 100
+    n_applications_per_period = 300
 elseif mode == "prod"
     n_simulations = 1000
     n_applications_per_period = 25
