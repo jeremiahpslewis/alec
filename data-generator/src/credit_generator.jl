ee67173da8c46f56418c161278457386ae4d12f1
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2021-08-28 09:58:23 +0200

    Limit n per trial

--- data-generator/src/credit_generator.jl
+++ data-generator/src/credit_generator.jl
@@ -16,7 +16,7 @@ mode = "test"
 
 if mode == "test"
     n_simulations = 9
-    n_applications_per_period = 250
+    n_applications_per_period = 100
 elseif mode == "prod"
     n_simulations = 30
     n_applications_per_period = 1000
